15411387d280ed3498ce3471013cab97f50c743a
heoh <h0h6h2h5@gmail.com>
2025-07-03 15:54:17 +0000

    games 분리

--- src/hooks/useGameList.ts
+++ src/hooks/useGameList.ts
@@ -1,24 +1,10 @@
-import type { Game } from '@/models/Game';
+import { games } from '@/models/Game';
 import { useBasename } from './useBasename';
 
 export function useGameList() {
   const basename = useBasename();
-  const games: Game[] = [
-    {
-      id: 'mind_match',
-      name: '통했어!',
-      imageUrl: `${basename}/images/game_mind_match.png`,
-    },
-    {
-      id: 'game1',
-      name: '게임1',
-      imageUrl: 'https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp',
-    },
-    {
-      id: 'game2',
-      name: '게임2',
-      imageUrl: 'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp',
-    },
-  ];
-  return games;
+  return games.map(game => ({
+    ...game,
+    imageUrl: game.imageUrl.startsWith('/') ? `${basename}${game.imageUrl}` : game.imageUrl,
+  }));
 }
--- src/models/Game.ts
+++ src/models/Game.ts
@@ -3,3 +3,21 @@ export type Game = {
   name: string;
   imageUrl: string;
 };
+
+export const games: Game[] = [
+  {
+    id: 'mind_match',
+    name: '통했어!',
+    imageUrl: '/images/game_mind_match.png',
+  },
+  {
+    id: 'game1',
+    name: '게임1',
+    imageUrl: 'https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp',
+  },
+  {
+    id: 'game2',
+    name: '게임2',
+    imageUrl: 'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp',
+  },
+];
